ea872cfb85374282b3b67ffb4676c114590eb92a
PretisH13 <mateo.prifti13@gmail.com>
2025-04-21 01:56:24 +0200

    create about,projects, contact and header .html and add django tag

--- portofolio_main/port_mat/urls.py
+++ portofolio_main/port_mat/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 
 urlpatterns = [ 
-     path('', views.home, name='home'), 
+     path('', views.home, name='home'),
+     path('about/', views.about, name='about'),
+     path('projects/', views.projects, name='projects'),
+     path('contact/', views.contact, name='contact'), 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- portofolio_main/port_mat/views.py
+++ portofolio_main/port_mat/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'port_mat/home.html')
+
+
+def about(request):
+    return render(request, 'port_mat/about.html')
+
+def projects(request):
+    return render(request, 'port_mat/projects.html')
+
+def contact(request):
+    return render(request, 'port_mat/contact.html')
