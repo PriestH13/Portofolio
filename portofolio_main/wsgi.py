96da7a1ebba38bdfd89d06f7be341cd7ed975322
PretisH13 <mateo.prifti13@gmail.com>
2025-05-02 01:19:45 +0200

    deploy

--- portofolio_main/wsgi.py
+++ portofolio_main/wsgi.py
@@ -1,9 +1,6 @@
-import sys
 import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio_main.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
