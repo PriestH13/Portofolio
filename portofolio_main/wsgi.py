43edcdecb2890620ce2d8dd36a279919809fc753
PretisH13 <mateo.prifti13@gmail.com>
2025-05-03 01:05:38 +0200

    test

--- portofolio_main/wsgi.py
+++ portofolio_main/wsgi.py
@@ -1,6 +1,10 @@
+# portofolio_main/wsgi.py
+
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Imposta il modulo delle impostazioni di Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio_main.settings')
 
+# Ottieni l'applicazione WSGI
 application = get_wsgi_application()
