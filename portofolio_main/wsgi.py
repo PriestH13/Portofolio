5383c671cfe11d74eef3a2633eb7d9823ae248e6
PretisH13 <mateo.prifti13@gmail.com>
2025-05-02 22:56:53 +0200

    test deploy

--- portofolio_main/wsgi.py
+++ portofolio_main/wsgi.py
@@ -3,19 +3,20 @@ import sys
 import logging
 from django.core.wsgi import get_wsgi_application
 
-# Configura logging
+# Configura il logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Aggiungi il percorso del progetto
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Imposta il modulo delle impostazioni
+# Imposta la variabile di ambiente per le impostazioni Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio_main.settings')
 
 try:
+    # Crea l'applicazione WSGI
     application = get_wsgi_application()
     logger.info("WSGI application started successfully.")
 except Exception as e:
     logger.error(f"Error starting WSGI application: {str(e)}")
-    raise
+    raise e
