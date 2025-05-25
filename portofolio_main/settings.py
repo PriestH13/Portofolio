7dd67d740366a7df9abee986d1a5966b754e0637
PriestH13 <mateo.prifti13@gmail.com>
2025-05-25 16:12:03 +0200

    fix double link issues for photos

--- portofolio_main/settings.py
+++ portofolio_main/settings.py
@@ -1,26 +1,12 @@
 import os
-import dj_database_url
 from pathlib import Path
 from decouple import config
-from dotenv import load_dotenv
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-dotenv_path = os.path.join(os.getcwd(), '.env')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS.extend(['localhost', '0.0.0.0'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -29,9 +15,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'port_mat.apps.PortMatConfig',
+
     'cloudinary',
-    'cloudinary_storage'
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +46,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
             ],
         },
     },
@@ -66,53 +53,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portofolio_main.wsgi.application'
 
-# Database configuration using DATABASE_URL
+import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
-# Internationalization
-LANGUAGE_CODE = 'en-us'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-TIME_ZONE = 'UTC'
+# Static files
 
-USE_I18N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-# This is your static folder that already exists
 STATICFILES_DIRS = [BASE_DIR / 'port_mat' / 'static']
-
-# This is where Django will collect static files in production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Email
 
-MEDIA_URL = 'https://res.cloudinary.com/{}/image/upload/'.format(os.getenv('CLOUDINARY_CLOUD_NAME'))
-
-
-# Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -121,12 +82,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
+# Google Analytics
+
 GOOGLE_ANALYTICS_ID = config('GOOGLE_ANALYTICS_ID')
-
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-
-CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
-CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
-CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
